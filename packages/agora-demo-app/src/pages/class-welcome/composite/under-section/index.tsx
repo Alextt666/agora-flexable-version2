e254c2ceca351c7d20fdc1d8aa8da57a0d939b69
alexzhang <alexzhangyaotian@gmail.com>
2024-05-13 16:12:25 +0800

    feat:录课功能模块配置

--- packages/agora-demo-app/src/pages/class-welcome/composite/under-section/index.tsx
+++ packages/agora-demo-app/src/pages/class-welcome/composite/under-section/index.tsx
@@ -1,14 +1,22 @@
 import { VideoIcon, FilesIcon } from '../icons';
 const UnderSection = () => {
+  const handleRecord = () => {
+    const ipc = window.require('electron').ipcRenderer;
+    // ipc.send('alex-start-record', 'main', 'alex');
+    console.log('record: ipc-instance-to-main', ipc);
+  };
+  const handleResource = () => {
+    console.log('resource: FE-fetch-api');
+  };
   return (
     <div className="fcr-mt-6 fcr-grid fcr-grid-cols-2 fcr-gap-6 fcr-text-white fcr-font-bold fcr-text-lg">
       <div className="sys-shadow fcr-flex fcr-flex-col fcr-gap-2 fcr-justify-center fcr-items-center fcr-cursor-pointer">
         <span>本地录课</span>
-        <img src={VideoIcon} alt="video" />
+        <img src={VideoIcon} alt="video" onClick={handleRecord} />
       </div>
       <div className="sys-shadow fcr-flex fcr-flex-col fcr-gap-2 fcr-justify-center fcr-items-center fcr-cursor-pointer">
         <span>课程资源</span>
-        <img src={FilesIcon} alt="video" />
+        <img src={FilesIcon} alt="video" onClick={handleResource} />
       </div>
     </div>
   );
