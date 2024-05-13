b5e4a50060a128b7352aa9fa02deda8514cc5018
alexzhang <alexzhangyaotian@gmail.com>
2024-05-13 16:24:21 +0800

    feat:录课功能模块位置调整

--- packages/agora-demo-app/src/pages/class-welcome/composite/under-section/index.tsx
+++ packages/agora-demo-app/src/pages/class-welcome/composite/under-section/index.tsx
@@ -10,13 +10,17 @@ const UnderSection = () => {
   };
   return (
     <div className="fcr-mt-6 fcr-grid fcr-grid-cols-2 fcr-gap-6 fcr-text-white fcr-font-bold fcr-text-lg">
-      <div className="sys-shadow fcr-flex fcr-flex-col fcr-gap-2 fcr-justify-center fcr-items-center fcr-cursor-pointer">
+      <div
+        onClick={handleRecord}
+        className="sys-shadow fcr-flex fcr-flex-col fcr-gap-2 fcr-justify-center fcr-items-center fcr-cursor-pointer">
         <span>本地录课</span>
-        <img src={VideoIcon} alt="video" onClick={handleRecord} />
+        <img src={VideoIcon} alt="video" />
       </div>
-      <div className="sys-shadow fcr-flex fcr-flex-col fcr-gap-2 fcr-justify-center fcr-items-center fcr-cursor-pointer">
+      <div
+        onClick={handleResource}
+        className="sys-shadow fcr-flex fcr-flex-col fcr-gap-2 fcr-justify-center fcr-items-center fcr-cursor-pointer">
         <span>课程资源</span>
-        <img src={FilesIcon} alt="video" onClick={handleResource} />
+        <img src={FilesIcon} alt="video" />
       </div>
     </div>
   );
