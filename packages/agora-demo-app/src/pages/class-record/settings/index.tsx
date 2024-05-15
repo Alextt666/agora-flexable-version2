5cbdc6c4a25e019e366384e988de6d5931d2da99
alexzhang <alexzhangyaotian@gmail.com>
2024-05-15 17:12:20 +0800

    feat:帧率&清晰度搭建

--- packages/agora-demo-app/src/pages/class-record/settings/index.tsx
+++ packages/agora-demo-app/src/pages/class-record/settings/index.tsx
@@ -13,7 +13,9 @@ export const SettingComp = (props: {
 
   return (
     <div className="fcr-flex fcr-justify-center fcr-items-center fcr-text-center">
-      <div className="fcr-w-1/4">left</div>
+      <div className="fcr-w-1/4">
+        <ResolutionComp></ResolutionComp>
+      </div>
       <div className="fcr-text-white fcr-flex-1 fcr-h-full fcr-flex fcr-items-center fcr-flex-col">
         <div className="fcr-flex fcr-w-full fcr-justify-evenly fcr-pt-4 fcr-h-1/3">
           {/* <div>课程名称: xxxx小学 </div> */}
@@ -37,4 +39,42 @@ export const SettingComp = (props: {
 const ImageIcon = (props: { className: string; iconSrc: string }) => {
   const className = props.className || '';
   return <img key={Math.random()} src={props.iconSrc} className={className} />;
+};
+
+const ResolutionComp = () => {
+  const RESOLUTIONS = ['1920 * 1080', '1280 * 720 ', '1024 * 768 '];
+  
+  return (
+    <div className="fcr-text-white">
+      <div className='fcr-flex fcr-justify-around fcr-items-center '>
+       
+        <div >
+        <div>清晰度</div>
+          {RESOLUTIONS.map((item, index) => {
+            return (
+              <div key={item} className="fcr-text-left">
+                <input type="radio" id={item} name="resolution" value={item} />
+                <label htmlFor={item} className="fcr-pl-2">
+                  {item}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+        <div >
+        <div>帧率/FPS</div>
+          {RESOLUTIONS.map((item, index) => {
+            return (
+              <div key={item} className="fcr-text-left">
+                <input type="radio" id={item} name="resolution" value={item} />
+                <label htmlFor={item} className="fcr-pl-2">
+                  {item}
+                </label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
